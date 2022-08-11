3a8f0cac60f1eb5851323e845bb35afbbb73b4e2
Michal Makowski <michmakow@gmail.com>
2022-08-11 11:53:22 +0200

    Add ValueRepositoryTests in DI

---
+++ MilleniumRecruitmentTask/Tests/ValueRepositoryTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MilleniumRecruitmentTask.Data.EFCore;
+
+namespace MilleniumRecruitmentTask.Tests
+{
+    [TestClass]
+    public class ValueRepositoryTests
+    {
+        private readonly EfCoreValueRepository valueRepository;
+
+        public ValueRepositoryTests()
+        {
+            var services = new ServiceCollection();
+            services.AddTransient<EfCoreValueRepository>();
+
+            var serviceProvider = services.BuildServiceProvider();
+
+            valueRepository = serviceProvider.GetService<EfCoreValueRepository>();         
+        }      
+    }
+}
